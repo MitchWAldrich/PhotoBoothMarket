62179b7452e153f531c1a1ed22a23fde944ba0d1
MitchWAldrich <mitch.w.aldrich@gmail.com>
2025-06-29 12:12:04 -0400

    added camera permissions and display logic

--- App.tsx
+++ App.tsx
@@ -6,30 +6,65 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import React from 'react';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 import {
   Camera,
+  CameraPermissionRequestResult,
   useCameraDevice,
-  // useCameraPermission,
 } from 'react-native-vision-camera';
+import CameraPermissions from './src/components/CameraPermission';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
-  // const { hasPermission, requestPermission } = useCameraPermission();
+
+  const camera = useRef<Camera>(null);
 
   const device = useCameraDevice('back');
+
+  // make dynamic to user obj
+  const [hasCameraPermission, setHasCameraPermission] =
+    useState<CameraPermissionRequestResult>('denied');
+
+  console.log('camera permission', hasCameraPermission);
   if (device == null) return;
   // if (!hasPermission) return <PermissionsPage />
-  // if (device == null) return <NoCameraDeviceError />
+  // if (device == null) return <NoCameraErrorView />
 
-  return (
+  if (hasCameraPermission === 'granted') {
+    const handleCameraPermissionsChange = (
+      bool: CameraPermissionRequestResult,
+    ) => {
+      setHasCameraPermission(bool);
+    };
+
+    return (
+      <View style={styles.container}>
+        <CameraPermissions onChangeCamera={handleCameraPermissionsChange} />
+        <Camera
+          ref={camera}
+          style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={true}
+          photo={true}
+        />
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <NewAppScreen templateFileName="App.tsx" />
+      </View>
+    );
+  } else {
     <View style={styles.container}>
-      <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
-  );
+      <Text>
+        In order to proceed, you will need to grant permission to your Camera!
+      </Text>
+    </View>;
+  }
 }
 
 const styles = StyleSheet.create({
