f38884e5d890816ce112e2c406a8fb7c53801a51
MitchWAldrich <mitch.w.aldrich@gmail.com>
2025-06-28 22:10:47 -0400

    added React Native Vision Camera to project

--- App.tsx
+++ App.tsx
@@ -6,13 +6,26 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
+import React from 'react';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  Camera,
+  useCameraDevice,
+  // useCameraPermission,
+} from 'react-native-vision-camera';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+  // const { hasPermission, requestPermission } = useCameraPermission();
+
+  const device = useCameraDevice('back');
+  if (device == null) return;
+  // if (!hasPermission) return <PermissionsPage />
+  // if (device == null) return <NoCameraDeviceError />
 
   return (
     <View style={styles.container}>
+      <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <NewAppScreen templateFileName="App.tsx" />
     </View>
