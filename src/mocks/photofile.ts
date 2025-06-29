a990b8efc05bf6c2b9d2b14f166a5a2ff9a34d81
MitchWAldrich <mitch.w.aldrich@gmail.com>
2025-06-29 18:33:10 -0400

    mock files for development

---
+++ src/mocks/photofile.ts
@@ -0,0 +1,10 @@
+import { PhotoFile } from "react-native-vision-camera";
+
+export const mockPhotoFile: PhotoFile = {
+  path: "/Users/mitchaldrich/NewDev/PhotoBoothMarket/src/assets/Adam-Sergison-Headshot.jpg",
+  width: 1080,
+  height: 1920,
+  isRawPhoto: true, 
+  orientation: "portrait", 
+  isMirrored: false,
+};
