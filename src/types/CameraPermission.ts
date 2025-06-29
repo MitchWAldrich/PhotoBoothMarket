b7997a5eb2194769f92ca0f01d6b5111c174fabb
MitchWAldrich <mitch.w.aldrich@gmail.com>
2025-06-29 18:37:22 -0400

    changed to Camera component

--- src/types/CameraPermission.ts
+++
@@ -1,5 +0,0 @@
-import { CameraPermissionRequestResult } from "react-native-vision-camera";
-
-export interface CameraPermissionProps {
-  onChangeCamera: (result: CameraPermissionRequestResult) => void;
-}
