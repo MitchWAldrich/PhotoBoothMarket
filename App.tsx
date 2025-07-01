5397a62378d319ca82a5d47d06156802e68fb141
MitchWAldrich <mitch.w.aldrich@gmail.com>
2025-06-30 22:16:47 -0400

    add camera roll permissions and saving functionality

--- App.tsx
+++ App.tsx
@@ -9,11 +9,14 @@ import React, { useState } from 'react';
 import {
   Button,
   Image,
+  PermissionsAndroid,
+  Platform,
   StatusBar,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
+import { CameraRoll } from '@react-native-camera-roll/camera-roll';
 import { appStyles } from './styles';
 import CameraComponent from './src/components/Camera';
 import { PhotoFile } from 'react-native-vision-camera';
@@ -23,6 +26,61 @@ function App() {
 
   const [isButtonPressed, setIsButtonPressed] = useState<boolean>(false);
   const [photo, setPhoto] = useState<PhotoFile | null>(null);
+
+  const hasAndroidPermission = async () => {
+    const getCheckPermissionPromise = () => {
+      if (Number(Platform.Version) >= 33) {
+        return Promise.all([
+          PermissionsAndroid.check(
+            PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+          ),
+          PermissionsAndroid.check(
+            PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+          ),
+        ]).then(
+          ([hasReadMediaImagesPermission, hasReadMediaVideoPermission]) =>
+            hasReadMediaImagesPermission && hasReadMediaVideoPermission,
+        );
+      } else {
+        return PermissionsAndroid.check(
+          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        );
+      }
+    };
+
+    const hasPermission = await getCheckPermissionPromise();
+    if (hasPermission) {
+      return true;
+    }
+    const getRequestPermissionPromise = () => {
+      if (Number(Platform.Version) >= 33) {
+        return PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+          PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+        ]).then(
+          statuses =>
+            statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES] ===
+              PermissionsAndroid.RESULTS.GRANTED &&
+            statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO] ===
+              PermissionsAndroid.RESULTS.GRANTED,
+        );
+      } else {
+        return PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        ).then(status => status === PermissionsAndroid.RESULTS.GRANTED);
+      }
+    };
+
+    return await getRequestPermissionPromise();
+  };
+
+  const savePicture = async () => {
+    if (Platform.OS === 'android' && !(await hasAndroidPermission())) {
+      return;
+    }
+
+    if (photo) CameraRoll.saveAsset(photo.path);
+  };
 
   const handleToggleCamera = (
     openState: boolean,
@@ -48,14 +106,22 @@ function App() {
         />
       )}
       {photo && (
-        <View style={appStyles.imageContainer}>
-          <Image
-            source={{
-              uri: `file://${photo.path}`,
-            }}
-            style={appStyles.fullImage}
+        <>
+          <View style={appStyles.imageContainer}>
+            <Image
+              source={{
+                uri: `file://${photo.path}`,
+              }}
+              style={appStyles.fullImage}
+            />
+          </View>
+          <Button
+            onPress={savePicture}
+            title="Save Photo"
+            color="teal"
+            accessibilityLabel="This button launches your phone's camera app"
           />
-        </View>
+        </>
       )}
 
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
