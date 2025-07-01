2de80f5b4c4f0c8df96299b79f2c8f42f7186b1f
MitchWAldrich <mitch.w.aldrich@gmail.com>
2025-07-01 15:37:14 -0400

    array of photofiles

---
+++ src/mocks/photofiles.ts
@@ -0,0 +1,31 @@
+import { PhotoFileWithID } from "../types/PhotoFileWithID";
+
+export const mockPhotoFiles: PhotoFileWithID[] = [
+  {
+    id: '1',
+    path: 'https://picsum.photos/200/300',
+    width: 1080,
+    height: 1920,
+    isRawPhoto: true, 
+    orientation: "portrait", 
+    isMirrored: false,
+  },
+  {
+    id: '2',
+    path: "https://picsum.photos/200/300",
+    width: 1080,
+    height: 1920,
+    isRawPhoto: true, 
+    orientation: "portrait", 
+    isMirrored: false,
+  },
+  {
+    id: '3',
+    path: "https://picsum.photos/200/300",
+    width: 1080,
+    height: 1920,
+    isRawPhoto: true, 
+    orientation: "portrait", 
+    isMirrored: false,
+  },
+];
