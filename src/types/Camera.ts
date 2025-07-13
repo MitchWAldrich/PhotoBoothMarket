94812445a8bb71189e232db57ee89c448237af87
MitchWAldrich <mitch.w.aldrich@gmail.com>
2025-07-13 18:44:09 -0400

    remove toggleCamera

--- src/types/Camera.ts
+++ src/types/Camera.ts
@@ -3,5 +3,4 @@ import { PhotoFile } from "react-native-vision-camera";
 export interface CameraComponentProps {
   passPhoto: (photograph: PhotoFile) => void;
   pressed: boolean;
-  toggleCamera: (openState: boolean) => void;
 }
