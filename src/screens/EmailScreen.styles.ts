927bd7f27704fcf4eca850fdaa9301d213c2c06f
MitchWAldrich <mitch.w.aldrich@gmail.com>
2025-08-06 23:56:08 -0400

    email styles

---
+++ src/screens/EmailScreen.styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const emailScreenStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#333',
+  },
+});
